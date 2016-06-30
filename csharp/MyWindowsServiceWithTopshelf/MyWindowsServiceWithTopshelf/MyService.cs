a591371cc65a4efd453eef500cd4c3274d6e244f
Philip Daniels <Philip.Daniels1971@gmail.com>
2016-06-30 16:05:17 +0100

    Add MyWindowsServiceWithTopshelf.

---
+++ csharp/MyWindowsServiceWithTopshelf/MyWindowsServiceWithTopshelf/MyService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyWindowsServiceWithTopshelf
+{
+    public class MyService
+    {
+        // We use a cancellation token to ensure that the service stops in a well-behaved manner.
+        private readonly CancellationTokenSource cancellationTokenSource;
+        private readonly CancellationToken cancellationToken;
+
+        // We have one job to run, we could of course have many.
+        private Task job;
+
+        public MyService()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            cancellationToken = cancellationTokenSource.Token;
+        }
+
+        public void Start()
+        {
+            job = Task.Run(() => DoWork(), cancellationToken);
+        }
+
+        public void Stop()
+        {
+            // Signal the task to stop.
+            cancellationTokenSource.Cancel();
+
+            // Then wait for the job. Could have a timeout here.
+            job.Wait();
+        }
+
+        public void DoWork()
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine("I am working");
+
+                Console.WriteLine("   Step 1");
+                Thread.Sleep(1000);
+
+                Console.WriteLine("   Step 2");
+                Thread.Sleep(1000);
+
+                Console.WriteLine("   Step 3");
+                Thread.Sleep(1000);
+
+                Console.WriteLine("   Step 4");
+                Thread.Sleep(1000);
+
+                Console.WriteLine("   Step 5");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ csharp/MyWindowsServiceWithTopshelf/MyWindowsServiceWithTopshelf/Program.cs
@@ -0,0 +1,41 @@
+﻿using Topshelf;
+
+namespace MyWindowsServiceWithTopshelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // See https://topshelf.readthedocs.io/en/latest/overview/commandline.html for details,
+            // but basically:
+            //     prog.exe install | start | stop | uninstall
+
+            HostFactory.Run(hostConfigurator =>
+            {
+                hostConfigurator.Service<MyService>(serviceConfigurator =>
+                {
+                    serviceConfigurator.ConstructUsing(() => new MyService());
+                    serviceConfigurator.WhenStarted(myService => myService.Start());
+                    serviceConfigurator.WhenStopped(myService => myService.Stop());
+                });
+
+                hostConfigurator.RunAsLocalSystem();
+
+                hostConfigurator.StartAutomaticallyDelayed();
+
+                // The string you need to pass is the one shown at the top of the Service Properties
+                // popup in Computer Management -> Services.
+                hostConfigurator.DependsOn("PlugPlay");
+
+                // Used to refer to the service by name in the SCM API, e.g. in DependsOn() above.
+                hostConfigurator.SetServiceName("MyWindowsServiceName");
+
+                // Appears as the leftmost column headed "Name" in Computer Management -> Services.
+                hostConfigurator.SetDisplayName("MyWindowsService Display Name");
+
+                // Appears as the column headed "Description" in Computer Management -> Services.
+                hostConfigurator.SetDescription("MyWindowsService using Topshelf");
+            });
+        }
+    }
+}
