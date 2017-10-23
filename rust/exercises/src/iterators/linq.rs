19fc3674b5f38723bc65321a0713a08dbc4f7b7b
Philip Daniels <Philip.Daniels1971@gmail.com>
2017-10-23 22:12:24 +0100

    Rust: more Iterator<Rect> examples.

---
+++ rust/exercises/src/iterators/linq.rs
@@ -0,0 +1,18 @@
+use std::slice::Iter;
+use std::iter::Filter;
+
+fn make_vec() -> Vec<i32> {
+    vec![4, 5, 6, -1, -100, 2, 400, 400, 500, 600, 11, 13, 15, 17, 19, 21, 23, 25]
+}
+
+pub fn run() {
+    println!("********* Iterator - missing Linq examples *********");
+    // demo_union();
+    // demo_single();
+    // demo_single_or_default();
+    // demo_intersect();
+    // demo_join();
+    // demo_first();
+    // demo_distinct();
+}
+
