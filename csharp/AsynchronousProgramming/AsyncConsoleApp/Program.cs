2ba2785988cb2fdafd4d65852d2d2ae8f74d28b3
Philip Daniels <Philip.Daniels1971@gmail.com>
2017-11-05 14:04:25 +0000

    Add a self-contained async console app example.

---
+++ csharp/AsynchronousProgramming/AsyncConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time = " + DateTime.Now);
+            // async void Main() doesn't work, the program needs to block until
+            // all async work has completed. This is the way to do it.
+            var result = DoSomethingAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Time = " + DateTime.Now);
+        }
+
+        static async Task<int> DoSomethingAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            var s = await DoSomethingElseAsync();
+            return Int32.Parse(s);
+        }
+
+        static async Task<string> DoSomethingElseAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            return "42";
+        }
+    }
+}
